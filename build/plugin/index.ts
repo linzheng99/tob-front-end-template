d27599e515f0476b894116d9d44f904d1bc1c47f
linzheng <alidlg9045@126.com>
2022-12-02 21:08:40 +0800

    Task List

--- build/plugin/index.ts
+++ build/plugin/index.ts
@@ -1,14 +1,10 @@
 import vue from '@vitejs/plugin-vue'
-import ComponentsPlugins from './components';
-import MockServer from './mock';
+import ComponentsPlugins from './components'
+import MockServer from './mock'
 
 export function setupVitePlugins(config) {
-  console.log(config);
-  
-  const plugins = [
-    vue(),
-    ComponentsPlugins(),
-    MockServer(config)
-  ]
+  console.log(config)
+
+  const plugins = [vue(), ComponentsPlugins(), MockServer(config)]
   return plugins
 }
