8eb5e1d15c00d93baac44bb6120e69d32e348fcc
linzheng <alidlg9045@126.com>
2022-12-19 22:59:29 +0800

    添加白名单路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,20 @@
-import type { App } from 'vue';
-import { createRouter, createWebHashHistory } from 'vue-router';
-import { routes } from './routes';
-import { TEST_ROUTER } from './routes/module/testRouter';
+import type { App } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { routes } from './routes'
+import { TEST_ROUTER } from './routes/module/testRouter'
 
 const basicRoutes: any = [...routes, TEST_ROUTER]
+
+// 白名单
+export const WHITE_PATH_LIST: string[] = []
+
+const getWhiteRouteList = (array: any[]) => {
+  array.forEach((item) => {
+    WHITE_PATH_LIST.push(item.path)
+    getWhiteRouteList(item.children || [])
+  })
+}
+getWhiteRouteList(basicRoutes)
 
 export const router = createRouter({
   history: createWebHashHistory(import.meta.env.VITE_PUBLIC_PATH),
@@ -13,5 +24,5 @@ export const router = createRouter({
 })
 
 export async function setupRouter(app: App) {
-  app.use(router);
+  app.use(router)
 }
