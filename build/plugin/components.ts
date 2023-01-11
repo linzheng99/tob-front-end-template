b2b5097260fbbf0d6837129b6527d223e8c856e2
linzheng <alidlg9045@126.com>
2023-01-11 23:57:58 +0800

    IconRender hooks

--- build/plugin/components.ts
+++ build/plugin/components.ts
@@ -1,12 +1,10 @@
-import Components from 'unplugin-vue-components/vite';
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 export default function ComponentsPlugins() {
   return Components({
     dts: 'src/typings/components.d.ts',
     types: [{ from: 'vue-router', names: ['RouterLink', 'RouterView'] }],
-    resolvers: [
-      NaiveUiResolver(),
-    ]
+    resolvers: [NaiveUiResolver()],
   })
 }
---
+++ src/hooks/component/useIconRender.ts
@@ -0,0 +1,40 @@
+import { h } from 'vue'
+import SvgIcon from '@/components/SvgIcon/index.vue'
+
+/**
+ * 图标渲染
+ * @param config
+ * @property icon - 图标名称(iconify图标的名称), 例如：mdi-account 或者 mdi:account
+ * @property localIcon - 本地svg图标文件名(assets/svg-icon文件夹下)
+ * @property color - 图标颜色
+ * @property fontSize - 图标大小
+ */
+interface IconConfig {
+  icon?: string
+  localIcon?: string
+  color?: string
+  fontSize?: number
+}
+
+interface IconStyle {
+  color?: string
+  fontSize?: string
+}
+
+/** 图标渲染 */
+export const useIconRender = () => {
+  const iconRender = (config: IconConfig) => {
+    const { icon, localIcon, color, fontSize } = config
+
+    const style: IconStyle = {}
+    color ? (style.color = color) : ''
+    fontSize ? (style.fontSize = `${fontSize}px`) : ''
+
+    // TODO 给一个默认icon
+    !icon && !localIcon ? console.warn('请输入有效 icon / localIcon!') : ''
+
+    return () => h(SvgIcon, { icon, localIcon, style })
+  }
+
+  return { iconRender }
+}
