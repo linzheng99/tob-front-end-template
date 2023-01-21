33ff5c7944e911e48af5ac2bbf049511fcbcb60a
linzheng <alidlg9045@126.com>
2023-01-21 21:27:30 +0800

    header collpase 组件

---
+++ src/layouts/default/Header/components/MenuCollapse.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="icon_size" @click="setSiderCollapse(!collpase)">
+    <SvgIcon v-if="!collpase" :icon="leftFold" />
+    <SvgIcon v-else :icon="rightFold" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SvgIcon from '@/components/SvgIcon/index.vue'
+import { useAppStoreWithOut } from '@/store/modules/app'
+import { computed } from 'vue'
+
+const appStore = useAppStoreWithOut()
+const { setSiderCollapse } = appStore
+
+const collpase = computed(() => appStore.getSiderCollapse)
+/** 展开 */
+const leftFold = 'line-md:menu-fold-left'
+/** 收起 */
+const rightFold = 'line-md:menu-fold-right'
+</script>
+
+<style lang="scss" scoped>
+.icon_size {
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
