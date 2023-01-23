5e34c17daec24c38238505d245ffdb6aed860e46
linzheng <alidlg9045@126.com>
2023-01-24 00:02:27 +0800

    update sider logo

--- src/layouts/default/Sidebar/components/SiderLogo.vue
+++ src/layouts/default/Sidebar/components/SiderLogo.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="sider_logo">
     <div v-if="!collapsed" class="sider_logo-unfold">
-      <img width="32" src="../../../../assets/images/logo.png" />
-      <h2 style="margin-left: 10px">admin</h2>
+      <img width="32" :src="logo" />
+      <h2 style="margin-left: 10px">template</h2>
     </div>
     <div v-else>
-      <img width="32" src="../../../../assets/images/logo.png" />
+      <img width="32" :src="logo" />
     </div>
   </div>
 </template>
@@ -13,6 +13,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useAppStoreWithOut } from '@/store/modules/app'
+import logo from '@/assets/images/logo.png'
 const appStore = useAppStoreWithOut()
 const collapsed = computed(() => appStore.getSiderCollapse)
 </script>
@@ -22,13 +23,14 @@ const collapsed = computed(() => appStore.getSiderCollapse)
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 52px;
+  height: 56px;
   font-size: 16px;
   color: white;
   width: 100%;
   &-unfold {
     display: flex;
     align-items: center;
+    font-weight: bold;
   }
 }
 </style>
