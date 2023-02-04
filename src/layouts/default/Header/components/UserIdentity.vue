3e6bc33ff4eea8a6d1fa02e214bc021c1b8dc84c
linzheng <alidlg9045@126.com>
2023-02-04 23:46:05 +0800

    header user

---
+++ src/layouts/default/Header/components/UserIdentity.vue
@@ -0,0 +1,51 @@
+<template>
+  <n-dropdown :options="options" @select="handleSelect">
+    <div class="identity">
+      <SvgIcon icon="mdi:user-circle-outline" style="font-size: 20px" />
+      <span style="padding-left: 8px">{{ userStore.userInfo.username }}</span>
+    </div>
+  </n-dropdown>
+</template>
+
+<script setup lang="ts">
+import SvgIcon from '@/components/SvgIcon/index.vue'
+import { useUserStoreWithOut } from '@/store/modules/user'
+import { useIconRender } from '@/hooks/component/useIconRender'
+const userStore = useUserStoreWithOut()
+const { iconRender } = useIconRender()
+const options = [
+  {
+    label: '退出登录',
+    key: 'logout',
+    icon: iconRender({
+      icon: 'humbleicons:logout',
+      color: 'white',
+    }),
+  },
+]
+const handleSelect = (key: string) => {
+  if (key === 'logout') {
+    logout()
+  }
+}
+
+const logout = () => {
+  userStore.logout()
+}
+</script>
+
+<style lang="scss" scoped>
+.identity {
+  color: white;
+  display: flex;
+  align-items: center;
+  height: 100%;
+  cursor: pointer;
+  padding-left: 12px;
+  padding-right: 12px;
+  border-radius: 4px;
+  &:hover {
+    background-color: rgb(121, 116, 164);
+  }
+}
+</style>
