ee1daf1e63e4bcdf143a2c611e1e1e630c992149
linzheng <alidlg9045@126.com>
2023-02-06 23:43:14 +0800

    sider routepush

--- src/layouts/default/Sidebar/components/SiderMenu.vue
+++ src/layouts/default/Sidebar/components/SiderMenu.vue
@@ -1,24 +1,30 @@
 <template>
   <n-config-provider :theme-overrides="themeOverrides">
     <n-menu
+      :value="activeKey"
       :collapsed="collapsed"
       :collapsed-width="64"
       :collapsed-icon-size="22"
       :options="option"
+      @update:value="handleUpdateMenu"
     />
   </n-config-provider>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import { usePermissionStoreWithOut } from '@/store/modules/permission'
 import { useAppStoreWithOut } from '@/store/modules/app'
 import { transformAuthRouteToMenu } from '@/router/helper/menuHelper'
 import { AppRouteRecordRaw } from '@/router/routeTypes'
 import { GlobalThemeOverrides } from 'naive-ui'
+import { useRouterPush } from '@/hooks/router/useRouterPush'
 
 const permissionStore = usePermissionStoreWithOut()
 const backMenuList = permissionStore.getBackMenuList
+const route = useRoute()
+const { routerPush } = useRouterPush()
 
 const appStore = useAppStoreWithOut()
 const collapsed = computed(() => appStore.getSiderCollapse)
@@ -50,6 +56,12 @@ const themeOverrides: GlobalThemeOverrides = {
     itemColorActive: 'rgba(220,238,98, 0.2)',
     itemColorActiveHover: 'rgba(220,238,98, 0.2)',
   },
+}
+
+const activeKey = computed(() => route.name)
+
+const handleUpdateMenu = (_key, item) => {
+  routerPush(item.routePath)
 }
 </script>
 
