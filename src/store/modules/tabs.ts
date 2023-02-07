30b187f36bae9685e0f2aac61daec8e15a867eac
linzheng <alidlg9045@126.com>
2023-02-07 10:37:33 +0800

    tabs store

---
+++ src/store/modules/tabs.ts
@@ -0,0 +1,51 @@
+import { defineStore } from 'pinia'
+import { store } from '../index'
+import { Tab, TabsState } from '@/layouts/default/Tabs/TabsType'
+import { getIndexInTabs } from '@/layouts/default/Tabs/util'
+import { useRouterPush } from '@/hooks/router/useRouterPush'
+
+export const useTabsStore = defineStore({
+  id: 'app-tabs',
+  state: (): TabsState => ({
+    tabs: [],
+    activeTab: '',
+  }),
+  getters: {
+    getTabs(): Tab[] {
+      return this.tabs
+    },
+    getActiveTab(): string {
+      return this.activeTab
+    },
+  },
+  actions: {
+    setTabs(tabs) {
+      this.tabs = tabs
+    },
+    addTabs(tab: Tab) {
+      const index = getIndexInTabs(this.tabs, this.activeTab)
+      if (index === -1) {
+        this.tabs.push(tab)
+      }
+    },
+    setActiveTab(value: any) {
+      this.activeTab = value
+    },
+    deleteTab(tab: Tab) {
+      const { routerPush } = useRouterPush(false)
+
+      const updateTabs = this.tabs.filter((item) => item.fullPath !== tab.fullPath)
+      this.tabs = updateTabs
+      const isActive = this.activeTab === tab.name
+      if (isActive) {
+        const selectedTab = this.tabs[this.tabs.length - 1]
+        this.setActiveTab(selectedTab.name)
+        routerPush(selectedTab.fullPath)
+      }
+    },
+  },
+})
+
+export function useTabsStoreWithOut() {
+  return useTabsStore(store)
+}
