52fa4beed3795aa7117e1d876c05cfe460ab5771
linzheng <alidlg9045@126.com>
2023-02-07 10:38:14 +0800

    layout header userActions

--- src/layouts/default/Header/index.vue
+++ src/layouts/default/Header/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="layout_header-wrapper">
     <MenuCollapse />
+    <UserActions />
   </div>
 </template>
 
 <script setup lang="ts">
 import MenuCollapse from './components/MenuCollapse.vue'
+import UserActions from './components/UserActions.vue'
 </script>
 
 <style lang="scss" scoped>
@@ -14,6 +16,7 @@ import MenuCollapse from './components/MenuCollapse.vue'
     height: 100%;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     padding: 10px;
     background-color: $theme-background-color;
   }
