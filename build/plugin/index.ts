26e2999b4f861dc9214c0a5d8b2c90e3e70ce56e
linzheng <alidlg9045@126.com>
2023-02-26 21:05:50 +0800

    修改 标签页 title icon

--- build/plugin/index.ts
+++ build/plugin/index.ts
@@ -2,8 +2,9 @@ import vue from '@vitejs/plugin-vue'
 import ComponentsPlugins from './components'
 import MockServer from './mock'
 import unplugins from './unplugins'
+import html from './html'
 
 export function setupVitePlugins(viteEnv: ImportMetaEnv) {
-  const plugins = [vue(), ComponentsPlugins(), ...unplugins(viteEnv), MockServer()]
+  const plugins = [vue(), ComponentsPlugins(), ...unplugins(viteEnv), MockServer(), html(viteEnv)]
   return plugins
 }
