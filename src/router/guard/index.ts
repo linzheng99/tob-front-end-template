96c749966ab4fc5231af90efe6d79fbf96455041
linzheng <alidlg9045@126.com>
2023-02-27 23:39:20 +0800

    create appinfo

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -1,11 +1,14 @@
 import type { Router } from 'vue-router'
 import { useTitle } from '@vueuse/core'
 import { createPermissionGuard } from './permissionGuard'
+import { useAppInfo } from '@/hooks/app/system'
+
+const { name } = useAppInfo()
 
 export function setupRouterGuard(router: Router) {
   createPermissionGuard(router)
   router.afterEach((to) => {
     // 设置document title
-    useTitle(to.meta.title as string)
+    useTitle(to.meta.title + ' - ' + name)
   })
 }
