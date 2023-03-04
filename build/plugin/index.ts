8e0ff5e2edfaec3de8390004360a9b1f0774f64b
linzheng <alidlg9045@126.com>
2023-03-04 19:50:06 +0800

    production mock inject

--- build/plugin/index.ts
+++ build/plugin/index.ts
@@ -3,14 +3,14 @@ import ComponentsPlugins from './components'
 import MockServer from './mock'
 import unplugins from './unplugins'
 import html from './html'
-import { devMode } from '../../src/utils/env'
 
-export function setupVitePlugins(viteEnv: ImportMetaEnv) {
-  const { VITE_HTTP_PROXY } = viteEnv
+export function setupVitePlugins(viteEnv: ImportMetaEnv, isBuild: boolean) {
+  const { VITE_USE_MOCK } = viteEnv
 
   const plugins = [vue(), ComponentsPlugins(), ...unplugins(viteEnv), html(viteEnv)]
 
-  VITE_HTTP_PROXY === devMode && plugins.push(MockServer())
+  // mock 模式
+  VITE_USE_MOCK && plugins.push(MockServer(isBuild))
 
   return plugins
 }
--- build/plugin/mock.ts
+++ build/plugin/mock.ts
@@ -1,8 +1,16 @@
 import { viteMockServe } from 'vite-plugin-mock'
 
-export default function MockServer() {
+export default function MockServer(isBuild: boolean) {
   return viteMockServe({
     mockPath: 'mock',
-    localEnabled: true,
+    // development
+    localEnabled: !isBuild,
+    // production
+    prodEnabled: isBuild,
+    injectCode: `
+      import { setupMockServer } from '../mock/index';
+
+      setupMockServer();
+      `,
   })
 }
