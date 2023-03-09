644c1bb262febddfee4a031fa06e1f7153ac24ad
linzheng <alidlg9045@126.com>
2023-03-09 10:21:05 +0800

    将UI样式移动到最外层（App.vue）

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,44 @@
 <script setup lang="ts">
-import { darkTheme } from 'naive-ui'
+import { darkTheme, GlobalThemeOverrides } from 'naive-ui'
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    primaryColor: '#dcee62',
+  },
+  Dropdown: {
+    color: 'rgb(100, 94, 147)',
+    optionColorHover: 'rgb(121, 116, 164)',
+    optionTextColor: 'white',
+    optionTextColorHover: 'white',
+  },
+  Menu: {
+    // 图标
+    itemIconColor: '#fff',
+    itemIconColorHover: '#fff',
+    itemIconColorActive: '#dcee62',
+    itemIconColorActiveHover: '#dcee62',
+    itemIconColorChildActive: '#dcee62',
+    itemIconColorChildActiveHover: '#dcee62',
+    itemIconColorCollapsed: '#fff',
+    // 文字
+    itemTextColor: '#fff',
+    itemTextColorHover: '#fff',
+    itemTextColorActive: '#dcee62',
+    itemTextColorActiveHover: '#dcee62',
+    itemTextColorChildActiveHover: '#dcee62',
+    // 下拉箭头
+    arrowColor: '#fff',
+    arrowColorHover: '#fff',
+    arrowColorChildActiveHover: '#dcee62',
+    itemColorHover: '#4c4e5e',
+    itemColorActive: 'rgba(220,238,98, 0.2)',
+    itemColorActiveHover: 'rgba(220,238,98, 0.2)',
+  },
+}
 </script>
 
 <template>
   <naive-provider>
-    <n-config-provider :theme="darkTheme">
+    <n-config-provider :theme="darkTheme" :theme-overrides="themeOverrides">
       <router-view />
     </n-config-provider>
   </naive-provider>
--- src/layouts/default/Sidebar/components/SiderMenu.vue
+++ src/layouts/default/Sidebar/components/SiderMenu.vue
@@ -1,14 +1,12 @@
 <template>
-  <n-config-provider :theme-overrides="themeOverrides">
-    <n-menu
-      :value="activeKey"
-      :collapsed="collapsed"
-      :collapsed-width="64"
-      :collapsed-icon-size="22"
-      :options="option"
-      @update:value="handleUpdateMenu"
-    />
-  </n-config-provider>
+  <n-menu
+    :value="activeKey"
+    :collapsed="collapsed"
+    :collapsed-width="64"
+    :collapsed-icon-size="22"
+    :options="option"
+    @update:value="handleUpdateMenu"
+  />
 </template>
 
 <script setup lang="ts">
@@ -17,7 +15,6 @@ import { useRoute } from 'vue-router'
 import { usePermissionStoreWithOut } from '@/store/modules/permission'
 import { transformAuthRouteToMenu } from '@/router/helper/menuHelper'
 import { AppRouteRecordRaw } from '@/router/routeTypes'
-import { GlobalThemeOverrides } from 'naive-ui'
 import { useRouterPush } from '@/hooks/router/useRouterPush'
 import { useAppLocalConfig } from '@/hooks/setting/useAppLocalConfig'
 
@@ -29,34 +26,6 @@ const { routerPush } = useRouterPush()
 const { getSiderCollapse } = useAppLocalConfig()
 const collapsed = computed(() => getSiderCollapse.value)
 const option = transformAuthRouteToMenu(backMenuList as AppRouteRecordRaw[])
-const themeOverrides: GlobalThemeOverrides = {
-  common: {
-    primaryColor: '#dcee62',
-  },
-  Menu: {
-    // 图标
-    itemIconColor: '#fff',
-    itemIconColorHover: '#fff',
-    itemIconColorActive: '#dcee62',
-    itemIconColorActiveHover: '#dcee62',
-    itemIconColorChildActive: '#dcee62',
-    itemIconColorChildActiveHover: '#dcee62',
-    itemIconColorCollapsed: '#fff',
-    // 文字
-    itemTextColor: '#fff',
-    itemTextColorHover: '#fff',
-    itemTextColorActive: '#dcee62',
-    itemTextColorActiveHover: '#dcee62',
-    itemTextColorChildActiveHover: '#dcee62',
-    // 下拉箭头
-    arrowColor: '#fff',
-    arrowColorHover: '#fff',
-    arrowColorChildActiveHover: '#dcee62',
-    itemColorHover: '#4c4e5e',
-    itemColorActive: 'rgba(220,238,98, 0.2)',
-    itemColorActiveHover: 'rgba(220,238,98, 0.2)',
-  },
-}
 
 const activeKey = computed(() => route.name)
 
