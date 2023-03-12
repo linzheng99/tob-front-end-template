749d43950c0b546494d070b3f160396360e7bfde
linzheng <alidlg9045@126.com>
2023-03-12 22:58:11 +0800

    add is fn

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -15,3 +15,23 @@ export function isFunction(val: unknown): val is Function {
 export function isString(val: unknown): val is string {
   return is(val, 'String')
 }
+
+export function isArray(val: any): val is Array<any> {
+  return val && Array.isArray(val);
+}
+
+export function isEmpty<T = unknown>(val: T): val is T {
+  if (isArray(val) || isString(val)) {
+    return val.length === 0;
+  }
+
+  if (val instanceof Map || val instanceof Set) {
+    return val.size === 0;
+  }
+
+  if (isObject(val)) {
+    return Object.keys(val).length === 0;
+  }
+
+  return false;
+}
