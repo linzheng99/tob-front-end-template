8e0797abde9268b759229bebc05b499899a4f1e7
linzheng <alidlg9045@126.com>
2023-03-23 23:24:31 +0800

    theme btn

--- src/layouts/default/Header/components/SettingButton.vue
+++ src/layouts/default/Header/components/SettingButton.vue
@@ -1,7 +1,28 @@
 <template>
-  <div></div>
+  <n-tooltip trigger="hover">
+    <template #trigger>
+      <div class="setting_button">
+        <SvgIcon icon="material-symbols:settings-rounded" />
+      </div>
+    </template>
+    主题配置
+  </n-tooltip>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.setting_button {
+  height: 100%;
+  color: #ffffff;
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  padding: 12px;
+  border-radius: 4px;
+  &:hover {
+    background-color: #333333;
+    cursor: pointer;
+  }
+}
+</style>
