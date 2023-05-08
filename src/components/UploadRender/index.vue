924a8576c04104ee44a79c074b85303d91c69e1d
linzheng <alidlg9045@126.com>
2023-05-08 23:53:46 +0800

    update uploadRender code

--- src/components/UploadRender/index.vue
+++ src/components/UploadRender/index.vue
@@ -9,34 +9,19 @@
           <span v-if="!files.length">请选择</span>
           <span style="display: flex" v-else>
             {{ files[0].name }}
-            <n-button
-              text
-              style="font-size: 18px; margin-left: 10px"
-              @click="deleteFile"
-              :render-icon="iconRender({ icon: 'ep:circle-close', color: 'gray' })"
-            />
+            <n-button text style="font-size: 18px; margin-left: 10px" @click="deleteFile"
+              :render-icon="iconRender({ icon: 'ep:circle-close', color: 'gray' })" />
           </span>
         </div>
         <div style="width: 88px">
-          <n-upload
-            ref="upload_ref"
-            action="https://naive-upload.free.beeceptor.com/"
-            :headers="{
-              'naive-info': 'hello!'
-            }"
-            :data="{
-              'naive-data': 'cool! naive!'
-            }"
-            :max="1"
-            :custom-request="customRequest"
-            :show-file-list="false"
-            v-model:file-list="files"
-          >
+          <n-upload ref="upload_ref" action="https://naive-upload.free.beeceptor.com/"
+            :headers="{ 'naive-info': 'hello!' }" :data="{ 'naive-data': 'cool! naive!' }" :max="1"
+            :custom-request="customRequest" :show-file-list="false" v-model:file-list="files">
             <n-button type="primary" style="height: 28px"> 选择文件 </n-button>
           </n-upload>
         </div>
       </div>
-      <n-button type="primary" :render-icon="iconRender({ icon: 'el:download' })">
+      <n-button type="primary" @click="download" :render-icon="iconRender({ icon: 'el:download' })">
         下载导入模板
       </n-button>
     </div>
@@ -50,9 +35,14 @@ import { ref, unref } from 'vue'
 const { iconRender } = useIconRender()
 const files: any = ref([])
 const upload_ref = ref(null)
+const emits = defineEmits(['download'])
 
 const customRequest = ({ file }) => {
   files.value.push(file)
+}
+
+const download = () => {
+  emits('download')
 }
 
 const deleteFile = () => {
@@ -60,6 +50,8 @@ const deleteFile = () => {
   dom.fileList.pop()
   files.value.pop()
 }
+
+defineExpose({ files })
 </script>
 
 <style scoped lang="scss">
@@ -68,22 +60,26 @@ const deleteFile = () => {
     width: 652px;
     height: 132px;
     padding: 23px 0px;
-    border: 1px solid red;
+    border-radius: 4px;
     background-color: #f2f5fa;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
   }
+
   &-text {
     font-size: 20px;
     color: #303133;
     font-weight: bold;
+    margin-bottom: 20px;
   }
+
   &-main {
     display: flex;
     align-items: center;
   }
+
   &-content {
     width: 380px;
     height: 40px;
@@ -95,6 +91,7 @@ const deleteFile = () => {
     align-items: center;
     margin-right: 20px;
   }
+
   &-file {
     display: flex;
     justify-content: center;
