d90aa72402927f980c9a3c0c716eb3a0c1052b0a
linzheng <alidlg9045@126.com>
2023-05-22 23:40:42 +0800

    test keep alive

--- src/views/system/accounts/index.vue
+++ src/views/system/accounts/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div id="app_content">账号</div>
+  <div id="app_content">账号<n-input v-model:value="accountsValue"></n-input> </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const accountsValue = ref('')
+</script>
 
 <style lang="scss" scoped></style>
--- src/views/system/auth/index.vue
+++ src/views/system/auth/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div id="app_content">权限</div>
+  <div id="app_content">权限 <n-input v-model:value="authValue"></n-input> </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const authValue = ref('')
+</script>
 
 <style lang="scss" scoped></style>
