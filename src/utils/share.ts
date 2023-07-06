5acff05076e1f70167985ad106d4c7fb1cb02659
linzheng <alidlg9045@126.com>
2023-07-06 23:06:28 +0800

    添加share文件

---
+++ src/utils/share.ts
@@ -0,0 +1,37 @@
+import { isObject } from './is'
+
+export function deepMerge<T = any>(source: any = {}, target: any = {}): T {
+  let key: string
+  for (key in target)
+    source[key] = isObject(source[key])
+      ? deepMerge(source[key], target[key])
+      : (source[key] = target[key])
+
+  return source
+}
+
+/**
+ * Add the object as a parameter to the URL
+ * @param baseUrl url
+ * @param obj
+ * @returns {string}
+ * eg:
+ *  let obj = {a: '3', b: '4'}
+ *  setObjToUrlParams('www.baidu.com', obj)
+ *  ==>www.baidu.com?a=3&b=4
+ */
+export function setObjToUrlParams(baseUrl: string, obj: any): string {
+  let parameters = ''
+  for (const key in obj) parameters += `${key}=${encodeURIComponent(obj[key])}&`
+
+  parameters = parameters.replace(/&$/, '')
+  return /\?$/.test(baseUrl) ? baseUrl + parameters : baseUrl.replace(/\/?$/, '?') + parameters
+}
+
+export const extend = Object.assign
+
+export const restRefsKey = (refs) => {
+  for (const [_key, ref] of Object.entries(refs)) {
+    (ref as any).value = null
+  }
+}
