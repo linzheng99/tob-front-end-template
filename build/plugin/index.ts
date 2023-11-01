aea6e0523f1ee6daa9fb76a813a31648395465c0
linzheng <alidlg9045@126.com>
2023-11-01 23:23:23 +0800

    一个特殊需求的插件(并不是最佳推荐)

--- build/plugin/index.ts
+++ build/plugin/index.ts
@@ -5,6 +5,8 @@ import MockServer from './mock'
 import html from './html'
 import svgIconsPlugin from './svgIcons'
 import imageMin from './imageMin'
+// 把全部资源打包到一个html文件里面 的插件
+// import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export function setupVitePlugins(viteEnv: ImportMetaEnv, isBuild: boolean) {
   const { VITE_USE_MOCK } = viteEnv
