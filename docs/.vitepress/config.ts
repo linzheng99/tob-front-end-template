3dd82620b10df45bdd138384cc1298917034fbcf
linzheng <alidlg9045@126.com>
2024-08-03 19:03:22 +0800

    base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,7 +6,7 @@ import { setupSidebar } from '../sidebar'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: './',
+  base: '/',
   title: 'tob Template',
   description: 'Document',
   head: [
