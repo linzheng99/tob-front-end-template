ca0bdf585e0fa4f96fc951394c361958ab7a542c
linzheng <alidlg9045@126.com>
2024-08-03 19:15:08 +0800

    fix workflow

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,7 +6,7 @@ import { setupSidebar } from '../sidebar'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/',
+  base: '/tob-frond-end-template/docs',
   title: 'tob Template',
   description: 'Document',
   head: [
