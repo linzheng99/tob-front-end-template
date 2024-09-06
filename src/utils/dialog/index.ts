5103550fcde2780bf6ef7e5ab8b8e6dd8e3f667d
linzheng <alidlg9045@126.com>
2024-09-06 13:53:52 +0800

    feat: 二次确认弹窗组件

---
+++ src/utils/dialog/index.ts
@@ -0,0 +1,45 @@
+interface CreateDialogType {
+  content: {
+    type: 'info' | 'success' | 'warning' | 'error'
+    title?: string
+    content?: string
+    positiveText?: string
+    negativeText?: string
+  }
+  onPositiveClick: () => Promise<void> | void
+  onNegativeClick?: () => Promise<void> | void
+}
+
+export function createDialog({ content: {
+  type,
+  title = '提示',
+  content = '你确定要删除吗？',
+  positiveText = '确定',
+  negativeText = '取消',
+}, onPositiveClick, onNegativeClick }: CreateDialogType) {
+  const dialog: any = window.$dialog?.[type]({
+    title,
+    content,
+    positiveText,
+    negativeText,
+    maskClosable: false,
+    onPositiveClick: async () => {
+      dialog.loading = true
+      try {
+        return await onPositiveClick()
+      }
+      catch (error) {
+        return false
+      }
+      finally {
+        dialog.loading = false
+      }
+    },
+    onNegativeClick: () => {
+      onNegativeClick?.()
+    },
+    onClose: () => {
+      onNegativeClick?.()
+    },
+  })
+}
